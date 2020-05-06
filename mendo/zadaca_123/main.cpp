c0b505c17431951e8357539972739fe5dfda1d4c
Filip Krstik <filip.krstik@gmail.com>
2020-05-06 16:03:22 +0200

    update

--- mendo/zadaca_123/main.cpp
+++ mendo/zadaca_123/main.cpp
@@ -4,6 +4,11 @@ using namespace std;
 
 int main()
 {
+    int broj;
     cout << "Hello world!" << endl;
+    cout << "Zdravo!"<<endl;
+    cout << "vnesi broj: "<<endl;
+    cin >> broj;
+    cout << broj;
     return 0;
 }
